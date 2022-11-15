fb17eaea26b421fa64350a6e76be930536ebcd0e
meet ribadiya <ribadiyameet1@gmail.com>
2022-11-15 17:23:32 +0530

    first commit

---
+++ routes/user.js
@@ -0,0 +1,19 @@
+const router = require("express").Router();
+
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+const User = require("../models/user");
+const auth = require('../middleware/auth')
+
+
+router.get('/', auth, (req, res) => {
+    User.findOne({ email: req.user.email }).select('-password').exec((err, user) => {
+        if (err) {
+            throw err;
+        }
+        res.send(user)
+    })
+})
+module.exports = router;
+
+
---
+++ server.js
@@ -0,0 +1,31 @@
+const express = require("express");
+const app = express();
+const port = 5000;
+const connection = require("./database/connection");
+const morgan = require("morgan");
+connection();
+
+const dotenv = require("dotenv");
+dotenv.config({ path: "config.env" });
+
+// log requests
+app.use(morgan("tiny"));
+
+app.use(express.json());
+
+const articlesRoutes = require("./routes/article");
+const registerRoute = require("./routes/register");
+const loginRoute = require("./routes/login");
+const userRoute = require('./routes/user');
+const refreshRoute = require('./routes/refresh');
+const logoutRoute = require('./routes/logout')
+
+
+app.use("/api/articles", articlesRoutes);
+app.use("/api/register", registerRoute);
+app.use("/api/login", loginRoute);
+app.use("/api/user", userRoute);
+app.use("/api/refresh", refreshRoute);
+app.use("/api/logout", logoutRoute);
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
